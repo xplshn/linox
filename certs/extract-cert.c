e22abaef0998102a22217f555957742e11d4c53f
xplshn <anto@xplshn.com.ar>
2026-03-24 04:49:21 +0000

    Apply oasis build-system patches (from v6.12.0)

--- certs/extract-cert.c
+++ certs/extract-cert.c
@@ -18,20 +18,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <err.h>
-#include <openssl/bio.h>
-#include <openssl/pem.h>
-#include <openssl/err.h>
-#if OPENSSL_VERSION_MAJOR >= 3
-# define USE_PKCS11_PROVIDER
-# include <openssl/provider.h>
-# include <openssl/store.h>
-#else
-# if !defined(OPENSSL_NO_ENGINE) && !defined(OPENSSL_NO_DEPRECATED_3_0)
-#  define USE_PKCS11_ENGINE
-#  include <openssl/engine.h>
-# endif
-#endif
-#include "ssl-common.h"
+#include <bearssl.h>
 
 #define PKEY_ID_PKCS7 2
 
@@ -44,92 +31,21 @@ void format(void)
 }
 
 static const char *key_pass;
-static BIO *wb;
 static char *cert_dst;
 static bool verbose;
 
-static void write_cert(X509 *x509)
+static void write_cert(void *dst_ctx, const void *src, size_t len)
 {
-	char buf[200];
+	FILE *dst = dst_ctx;
 
-	if (!wb) {
-		wb = BIO_new_file(cert_dst, "wb");
-		ERR(!wb, "%s", cert_dst);
-	}
-	X509_NAME_oneline(X509_get_subject_name(x509), buf, sizeof(buf));
-	ERR(!i2d_X509_bio(wb, x509), "%s", cert_dst);
-	if (verbose)
-		fprintf(stderr, "Extracted cert: %s\n", buf);
-}
-
-static X509 *load_cert_pkcs11(const char *cert_src)
-{
-	X509 *cert = NULL;
-#ifdef USE_PKCS11_PROVIDER
-	OSSL_STORE_CTX *store;
-
-	if (!OSSL_PROVIDER_try_load(NULL, "pkcs11", true))
-		ERR(1, "OSSL_PROVIDER_try_load(pkcs11)");
-	if (!OSSL_PROVIDER_try_load(NULL, "default", true))
-		ERR(1, "OSSL_PROVIDER_try_load(default)");
-
-	store = OSSL_STORE_open(cert_src, NULL, NULL, NULL, NULL);
-	ERR(!store, "OSSL_STORE_open");
-
-	while (!OSSL_STORE_eof(store)) {
-		OSSL_STORE_INFO *info = OSSL_STORE_load(store);
-
-		if (!info) {
-			drain_openssl_errors(__LINE__, 0);
-			continue;
-		}
-		if (OSSL_STORE_INFO_get_type(info) == OSSL_STORE_INFO_CERT) {
-			cert = OSSL_STORE_INFO_get1_CERT(info);
-			ERR(!cert, "OSSL_STORE_INFO_get1_CERT");
-		}
-		OSSL_STORE_INFO_free(info);
-		if (cert)
-			break;
-	}
-	OSSL_STORE_close(store);
-#elif defined(USE_PKCS11_ENGINE)
-		ENGINE *e;
-		struct {
-			const char *cert_id;
-			X509 *cert;
-		} parms;
-
-		parms.cert_id = cert_src;
-		parms.cert = NULL;
-
-		ENGINE_load_builtin_engines();
-		drain_openssl_errors(__LINE__, 1);
-		e = ENGINE_by_id("pkcs11");
-		ERR(!e, "Load PKCS#11 ENGINE");
-		if (ENGINE_init(e))
-			drain_openssl_errors(__LINE__, 1);
-		else
-			ERR(1, "ENGINE_init");
-		if (key_pass)
-			ERR(!ENGINE_ctrl_cmd_string(e, "PIN", key_pass, 0), "Set PKCS#11 PIN");
-		ENGINE_ctrl_cmd(e, "LOAD_CERT_CTRL", 0, &parms, NULL, 1);
-		ERR(!parms.cert, "Get X.509 from PKCS#11");
-		cert = parms.cert;
-#else
-		fprintf(stderr, "no pkcs11 engine/provider available\n");
-		exit(1);
-#endif
-	return cert;
+	if (fwrite(src, 1, len, dst) != len)
+		err(1, "write %s", cert_dst);
 }
 
 int main(int argc, char **argv)
 {
 	char *cert_src;
 	char *verbose_env;
-
-	OpenSSL_add_all_algorithms();
-	ERR_load_crypto_strings();
-	ERR_clear_error();
 
 	verbose_env = getenv("KBUILD_VERBOSE");
 	if (verbose_env && strchr(verbose_env, '1'))
@@ -146,37 +62,45 @@ int main(int argc, char **argv)
 	if (!cert_src[0]) {
 		/* Invoked with no input; create empty file */
 		FILE *f = fopen(cert_dst, "wb");
-		ERR(!f, "%s", cert_dst);
+		if (!f)
+			err(1, "%s", cert_dst);
 		fclose(f);
 		exit(0);
 	} else if (!strncmp(cert_src, "pkcs11:", 7)) {
-		X509 *cert = load_cert_pkcs11(cert_src);
-
-		ERR(!cert, "load_cert_pkcs11 failed");
-		write_cert(cert);
+		errx(1, "PKCS#11 certificates are not supported");
 	} else {
-		BIO *b;
-		X509 *x509;
+		FILE *src, *dst;
+		br_pem_decoder_context ctx;
+		char buf[8192], *pos;
+		size_t len = 0, n;
 
-		b = BIO_new_file(cert_src, "rb");
-		ERR(!b, "%s", cert_src);
+		src = fopen(cert_src, "rb");
+		if (!src)
+			err(1, "open %s", cert_src);
+		dst = fopen(cert_dst, "wb");
+		if (!dst)
+			err(1, "open %s", cert_dst);
 
-		while (1) {
-			x509 = PEM_read_bio_X509(b, NULL, NULL, NULL);
-			if (wb && !x509) {
-				unsigned long err = ERR_peek_last_error();
-				if (ERR_GET_LIB(err) == ERR_LIB_PEM &&
-				    ERR_GET_REASON(err) == PEM_R_NO_START_LINE) {
-					ERR_clear_error();
+		br_pem_decoder_init(&ctx);
+		br_pem_decoder_setdest(&ctx, write_cert, dst);
+		for (;;) {
+			if (len == 0) {
+				if (feof(src))
 					break;
-				}
+				len = fread(buf, 1, sizeof(buf), src);
+				if (ferror(src))
+					err(1, "read %s", cert_src);
+				pos = buf;
 			}
-			ERR(!x509, "%s", cert_src);
-			write_cert(x509);
+			n = br_pem_decoder_push(&ctx, pos, len);
+			pos += n;
+			len -= n;
+			if (br_pem_decoder_event(&ctx) == BR_PEM_ERROR)
+				errx(1, "PEM decode failure");
 		}
+		if (fflush(dst) != 0)
+			err(1, "flush %s", cert_dst);
 	}
-
-	BIO_free(wb);
 
 	return 0;
 }
