b88c66fd545d777b51f7d03c98fd0caada402ca1
xplshn <anto@xplshn.com.ar>
2026-04-01 05:18:31 +0000

    Apply oasis build-system patches (from v6.12.0)

--- tools/perf/trace/beauty/sndrv_pcm_ioctl.sh
+++ tools/perf/trace/beauty/sndrv_pcm_ioctl.sh
@@ -4,6 +4,6 @@
 [ $# -eq 1 ] && beauty_uapi_sound_dir=$1 || beauty_uapi_sound_dir=tools/perf/trace/beauty/include/uapi/sound/
 
 printf "static const char *sndrv_pcm_ioctl_cmds[] = {\n"
-grep "^#define[\t ]\+SNDRV_PCM_IOCTL_" $beauty_uapi_sound_dir/asound.h | \
-	sed -r 's/^#define +SNDRV_PCM_IOCTL_([A-Z0-9_]+)[\t ]+_IO[RW]*\( *.A., *(0x[[:xdigit:]]+),?.*/\t[\2] = \"\1\",/g'
+grep "^#define[	 ]\+SNDRV_PCM_IOCTL_" $beauty_uapi_sound_dir/asound.h | \
+	sed -E 's/^#define +SNDRV_PCM_IOCTL_([A-Z0-9_]+)[	 ]+_IO[RW]*\( *.A., *(0x[[:xdigit:]]+),?.*/	[\2] = \"\1\",/g'
 printf "};\n"
